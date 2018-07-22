f4501b5066362c2a8b5b47f4ff0e99f8d9007fb3
Morteza Allahpour <mortezaallahpour@outlook.com>
2018-07-22 13:43:34 +0430

    finish getTranslationFor

--- src/Translatable.php
+++ src/Translatable.php
@@ -10,25 +10,105 @@ namespace Mortza\Translatable;
  * then define parameters listed below in class
  * with this package there is no limitation on number of translation for each attribute except database limitations
  *
+ * to enable translation for a model you should add attribute names you want to translate to the
+ * `protected $trans_attributes` array. attributes tagged for translation should be json column on database.
+ *
+ * example:
+ *
+ * ```php
+ * protected $trans_attributes = ['description','name',];
+ * ```
+ *
+ * plus you should add these values to `protected $casts` to cast to array
+ *
+ * example:
+ *
+ * ```php
+ * protected $casts = ['description'=>'array','name'=>'array',];
+ * ```
  * @package Mortza\Translatable
+ *
  */
 trait Translatable
 {
     /**
-     * @param $attr
-     * @param $lang
+     * to get translation for attribute `$attr` on model call this method
+     * by default fallback language is `en` if you want to override it
+     * you can pass third parameter `$fall_back`. of course its cumbersome to define
+     * fallback for each call so you can define `protected $fall_back_lang;` so this
+     * function ignore third parameter. remember that model fall_back language has
+     * higher priority than this argument.
+     *
+     * @param string $attr
+     * @param string $lang
      * @param string $fall_back
+     * @return string
      */
     public function getTranslationFor($attr, $lang, $fall_back = 'en')
     {
+        // boot function, check various conditions
+        $this->translatableBoot($attr, $lang, $fall_back);
+        if ($this->translationExist($attr, $lang)) {
+            // simply return value from array :)
+            return $this->{$attr}[$lang];
+        } else if ($this->translationExist($attr, $fall_back)) {
+            // return fall_back translation
+            return $this->{$attr}[$fall_back];
+        }
+    }
 
+    private function setFallBack(&$fall_back)
+    {
+        // if model defined fall_back property we use it, unless use function argument
+        if (property_exists($this, "fall_back"))
+            $fall_back = $this->fall_back;
+    }
+
+    private function translationExist($attr, $lang)
+    {
+        // determines if $this->{$attr} has key named $lang
+        return in_array($this->{$attr}, $lang);
+    }
+
+    private function translatableBoot($attr, $lang, &$fall_back)
+    {
+        if (!$this->checkPropExist($attr))
+            throw \Exception('Attribute not found on model or not listed on $trans_attributes');
+        // check data types
+        $this->checkDataType($attr, $lang, $fall_back);
+        $this->setFallBack($fall_back);
+    }
+
+    private function checkPropExist($attr)
+    {
+        return property_exists($this, $attr) && in_array($this->trans_attributes, $attr);
+    }
+
+    private function checkDataType($attr, $lang, $fall_back)
+    {
+        // check if $attr is string
+        if (!is_string($attr))
+            throw \Exception('$attr should be string');
+        // check if $lang is string
+        if (!is_string($lang))
+            throw \Exception('$attr should be string');
+        // check if $fall_back is string
+        if (!is_string($fall_back))
+            throw \Exception('$attr should be string');
+        // check if $this->attr is array
+        if (!is_array($this->{$attr}))
+            throw \Exception("$attr should be array, maybe you forgot to add it to \$casts");
     }
 
     /**
-     * @param $attr
-     * @param $lang
+     * this function get a attribute name, language code and its value and set translation for that attribute
+     * attribute must be listed on `$trans_attributes`
+     *
+     * @param string $attr
+     * @param string $lang
+     * @param string $value
      */
-    public function setTranslationFor($attr, $lang)
+    public function setTranslationFor($attr, $lang, $value)
     {
 
     }
