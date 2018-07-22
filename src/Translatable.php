b374c380f722c20025c0642aa9d162b3fc05af2a
Morteza Allahpour <mortezaallahpour@outlook.com>
2018-07-22 11:31:05 +0430

    edit .gitignore file

--- src/Translatable.php
+++ src/Translatable.php
@@ -2,5 +2,34 @@
 
 namespace Mortza\Translatable;
 
-trait Translatable{
+/**
+ * Trait Translatable
+ *
+ * with this package you can add translation to laravel models
+ * to setup translation you should add this trait to your Model.php file
+ * then define parameters listed below in class
+ * with this package there is no limitation on number of translation for each attribute except database limitations
+ *
+ * @package Mortza\Translatable
+ */
+trait Translatable
+{
+    /**
+     * @param $attr
+     * @param $lang
+     * @param string $fall_back
+     */
+    public function getTranslationFor($attr, $lang, $fall_back = 'en')
+    {
+
+    }
+
+    /**
+     * @param $attr
+     * @param $lang
+     */
+    public function setTranslationFor($attr, $lang)
+    {
+
+    }
 }
